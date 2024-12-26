6eefd543d53294589e949270b82f598380f53258
Asitha Kanchana Palliyaguru <114346362+AsithaKanchana1@users.noreply.github.com>
2024-12-26 22:29:12 +0530

    finalized

--- AnimatedScatterPlot/main.py
+++ AnimatedScatterPlot/main.py
@@ -4,18 +4,17 @@ data = px.data.gapminder()
 
 fig = px.scatter(
     data,
-    x = "gdpPercap",
-    y = "lifeExp",
-    animation_frame = "year",
-    animation_grupe = "country",
-    size = "pop",
-    color = "continent",
-    hover_name = "country",
-    log_x = True,
-    size_max = 55,
-    range_x = [200, 60000],
-    range_y = [25, 90],
-    title = "Animater Scatter Plot : Life Expectency vs GOP Per Capita"
-    
+    x="gdpPercap",
+    y="lifeExp",
+    animation_frame="year",
+    animation_group="country",  # Corrected argument
+    size="pop",
+    color="continent",
+    hover_name="country",
+    log_x=True,
+    size_max=55,
+    range_x=[200, 60000],
+    range_y=[25, 90],
+    title="Animated Scatter Plot: Life Expectancy vs GDP Per Capita"
 )
 fig.show()
